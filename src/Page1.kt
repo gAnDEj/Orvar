0aca19468cb57b22d958ba3fe7ac0a5669a45fb4
Robin Blondin <robin.blondin@protonmail.com>
2024-07-06 21:44:02 +0200

    Added more story

--- src/Page1.kt
+++ src/Page1.kt
@@ -26,7 +26,24 @@ fun Page1() {
                 "Erik 'My uncle is selling an RV for 5000kr. I could try to get you discount. That way you can get out of here.'\n" +
                 "Orvar 'Yeah but it's pretty unrealistic to just disappear. I would be found eventually anyway'\n" +
                 "Erik 'Yeah true, change your name and move up to the mountains or something. You can still live in the RV.'\n" +
-                "Orvar 'You know what, I'll buy it and move out and go from there. Thanks" +
+                "Orvar 'You know what, I'll buy it and move out and go from there. Thanks\n" +
+                "Erik 'No worries. Always here to help! Anyway gotta keep working. See you later.'\n" +
+                "Orvar 'Take care!'\n" +
+                "\nYou finish your shift like usual and bike home. \nUnsurprisingly you see your mom standing outside waiting for you. \n\n" +
+                "Orvar 'Hey mom...'\n" +
+                "Mom 'What was that all about?'\n" +
+                "Orvar 'Look mom I'm sorry...'\n" +
+                "Mom 'I don't want to hear it. How do you think we're gonna get food if you're like this?'\n" +
+                "Orvar 'You know what?! I'm tired of working my ass off for you to eat and drink all the money up and do nothing. Ever since dad died you haven't worked a single day. \n'" +
+                "Mom 'Dont ever talk to me like that ever again, you hear me?!'\n" +
+                "Orvar 'Can't wait to get out of here.'\n" +
+                "Mom 'You're never leaving on my watch!'\n" +
+                "Orvar 'Whatever.'\n\n" +
+                "You go to your room in anger. You sit on your bed staring at your wall when you remember that RV Erik's uncle is selling.\n\n" +
+                "(Next week I'll buy that RV and get a lighter and a hatchet from the basement then I'll get out of here.) \n\n" +
+                "One week goes by...\n\n" +
+                "" +
+                "" +
                 "" +
                 "" +
                 "" +
